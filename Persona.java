ad930d3811a253c8b1c9b7fa778700366f68404f
Kigo524 <kigocode@gmail.com>
2026-02-17 23:41:26 -0600

    Agregar clase abstracta Persona con atributos y métodos básicos

---
+++ Persona.java
@@ -0,0 +1,16 @@
+public abstract class Persona {
+
+    private String nombre;
+    private String ine;
+    private String email;
+
+    public Persona(String nombre, String ine, String email){
+        this.nombre = nombre;
+        this.ine = ine;
+        this.email = email;
+    }
+
+    //getters y setters
+    public String getNombre() {return nombre;}
+    public String getEmail() {return email;}
+}
