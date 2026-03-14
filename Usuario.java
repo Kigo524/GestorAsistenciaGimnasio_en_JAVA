031e11f9ff2649c05b83eb4b9a6e01d38abc7c72
Kigo524 <kigocode@gmail.com>
2026-03-14 17:46:38 -0600

    Mejorar comentarios y mensajes en la clase Usuario para mayor claridad en el registro de asistencias y aplicación de descuentos.

--- Usuario.java
+++ Usuario.java
@@ -1,6 +1,9 @@
+/*Representa a un socio activo del gimnasio
+Gestiona su membresia y su historial de fidelizacion por asistencia */
+
 public class Usuario extends Persona{
     
-    //asistencias necesarias para el descuento
+    //asistencias necesarias para el descuento. Se pueden cambiar desde aqui.
     private static final int META_ASISTENCIA = 20;
     private static final double DESCUENTO_PREMIO = 10.0;
 
@@ -13,19 +16,32 @@ public class Usuario extends Persona{
         this.asistenciasSeguidas = 0; //para comenzar la suscripcion
     }
 
+    /*Registrar una visita y verificar automaticamente si el usuario es acreedor 
+    a un beneficio por su constancia */
+
     public void registrarAsistencia(){
         this.asistenciasSeguidas++;
-        System.out.println("Asistencia registrada: " + getNombre());
+        System.out.println("Asistencia #"+ asistenciasSeguidas+ " registrada: "+ getNombre());
+
+        //aqui el sistema checa el premio
+        verificarPremio();
+    }
+
+    /*El metodo es privado porque la propia clase debe decidir cuando dar premios */
+    private void verificarPremio(){
+        if (this.asistenciasSeguidas >= META_ASISTENCIA) {
+            System.out.println("Felicidades! Por tu constacia se te ha aplicado un "+ DESCUENTO_PREMIO+ "% de descuento.");
+            this.membresia.aplicarDescuento(DESCUENTO_PREMIO);
+            this.asistenciasSeguidas = 0; //para reiniciar el contador despues de ganar el descuento
+        }
     }
 
     public Membresia getMembresia() {return membresia;}
     public int getAsistenciasSeguidas() {return asistenciasSeguidas;}
 
-    public void verificarPremio(){
-        if (this.asistenciasSeguidas >= META_ASISTENCIA) {
-            System.out.println("Felicidades! Has ganado un descuento!");
-            this.getMembresia().aplicarDescuento(DESCUENTO_PREMIO);
-            this.asistenciasSeguidas = 0; //para reiniciar el contador despues de ganar el descuento
-        }
+    @Override
+    public String toString(){
+        return String.format("Usuario: %s | INE: %s | Asistencias: %d | Mmebresia: %s",
+            getNombre(), getIne(), asistenciasSeguidas, membresia.getTipo());
     }
 }
